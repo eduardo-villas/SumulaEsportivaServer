d6fdc6d6202eb683054dd2889326ce4dc157f1d9
Wilde Rossi <wilde.rossi@db1.com.br>
2017-06-16 16:47:22 -0300

    Adicionando rota para visualizar console do h2

--- src/main/java/br/com/sumulaesportiva/App.java
+++ src/main/java/br/com/sumulaesportiva/App.java
@@ -2,12 +2,21 @@ package br.com.sumulaesportiva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-	
+
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
+	}
 }
