dc3a867042da13053fcdde2c642a69b057e23a97
Wilde <gutopicciani@yahoo.com.br>
2017-06-17 14:41:39 -0300

    Adicionando Spring data rest ao projeto

--- src/main/java/br/com/sumulaesportiva/App.java
+++ src/main/java/br/com/sumulaesportiva/App.java
@@ -1,8 +1,8 @@
 package br.com.sumulaesportiva;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
---
+++ src/main/java/br/com/sumulaesportiva/controllers/HelloWorldController.java
@@ -0,0 +1,30 @@
+package br.com.sumulaesportiva.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sumulaesportiva.entities.Employee;
+import br.com.sumulaesportiva.repositories.EmployeeRepository;
+
+@ResponseBody
+@RestController
+public class HelloWorldController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@RequestMapping("/helloworld")
+	public List<Employee> hello() {
+		Employee employee = new Employee("Wilde", "Rossi", "Teste");
+		employeeRepository.save(employee);
+		System.out.println(employeeRepository.count());
+		List<Employee> list = new ArrayList<>();
+		employeeRepository.findAll().forEach(list::add);
+		return list;
+	}
+}
